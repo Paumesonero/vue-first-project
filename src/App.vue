5457702edba8c0c06a5d8573099e9e005285b071
Pau Mesonero <paumesonero@Paus-MacBook-Air.local>
2023-04-22 23:27:04 +0200

    Card component added

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <script setup>
 import quizData from "./data/quizes.json";
+import Card from "./components/Card.vue";
 import { ref, watch } from "vue";
 
 const quizes = ref(quizData);
@@ -20,13 +21,7 @@ watch(search, () => {
       <input v-model.trim="search" type="text" placeholder="Search..." />
     </header>
     <div class="options-container">
-      <div class="card" v-for="quiz in quizes" :key="quiz.id">
-        <img :src="quiz.img" alt="" />
-        <div class="card-text">
-          <h2>{{ quiz.name }}</h2>
-          <p>{{ quiz.questions.length }} questions</p>
-        </div>
-      </div>
+      <Card v-for="quiz in quizes" :key="quiz.id" :quizProp="quiz" />
     </div>
   </div>
 </template>
@@ -60,31 +55,5 @@ header input {
   display: flex;
   flex-wrap: wrap;
   margin-top: 40px;
-}
-
-/* CARD */
-
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-}
-
-.card img {
-  width: 100%;
-  height: 190px;
-  margin: 0;
-}
-
-.card .card-text {
-  padding: 0 5px;
-}
-
-.card .card-text h2 {
-  font-weight: bold;
 }
 </style>
---
+++ src/components/Card.vue
@@ -0,0 +1,39 @@
+<script setup>
+// import { defineProps } from "vue";
+
+const { quizProp } = defineProps(["quizProp"]);
+</script>
+<template>
+  <div class="card">
+    <img :src="quizProp.img" alt="quiz image" />
+    <div class="card-text">
+      <h2>{{ quizProp.name }}</h2>
+      <p>{{ quizProp.questions.length }} questions</p>
+    </div>
+  </div>
+</template>
+<style scoped>
+.card {
+  width: 310px;
+  overflow: hidden;
+  border-radius: 2%;
+  box-shadow: 1px 1px 10px rgba(0, 0, 0, 0.1);
+  margin-bottom: 35px;
+  margin-right: 20px;
+  cursor: pointer;
+}
+
+.card img {
+  width: 100%;
+  height: 190px;
+  margin: 0;
+}
+
+.card .card-text {
+  padding: 0 5px;
+}
+
+.card .card-text h2 {
+  font-weight: bold;
+}
+</style>
