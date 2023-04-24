825b4e3f3d298cf304bd7046229df6101d73f61e
Pau Mesonero <paumesonero@Paus-MacBook-Air.local>
2023-04-24 20:25:49 +0200

    questions show dynamically

--- src/components/CardQuiz.vue
+++ src/components/CardQuiz.vue
@@ -1,5 +1,21 @@
+<script setup>
+import Question from "./Question.vue";
+import Header from "./QuizHeader.vue";
+import { useRoute } from "vue-router";
+import { ref } from "vue";
+import quizes from "../data/quizes.json";
+const route = useRoute();
+
+const quizId = Number(route.params.id);
+const currentQuestionIndex = ref(0);
+const quiz = quizes.find((el) => el.id === quizId);
+</script>
+
 <template>
   <div>
-    <h2>You are going good</h2>
+    <Header />
+    <div>
+      <Question :question="quiz.questions[currentQuestionIndex]" />
+    </div>
   </div>
 </template>
