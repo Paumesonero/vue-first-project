c11df7863e40e79ab50593bb639c4fb3e7475cff
Pau Mesonero <paumesonero@Paus-MacBook-Air.local>
2023-04-24 20:45:13 +0200

    dynamic header working

--- src/components/CardQuiz.vue
+++ src/components/CardQuiz.vue
@@ -2,20 +2,31 @@
 import Question from "./Question.vue";
 import Header from "./QuizHeader.vue";
 import { useRoute } from "vue-router";
-import { ref } from "vue";
+import { ref, watch } from "vue";
 import quizes from "../data/quizes.json";
 const route = useRoute();
 
 const quizId = Number(route.params.id);
 const currentQuestionIndex = ref(0);
 const quiz = quizes.find((el) => el.id === quizId);
+
+const questionStatus = ref(
+  `${currentQuestionIndex.value}/${quiz.questions.length}`
+);
+watch(
+  () => currentQuestionIndex.value,
+  () => {
+    questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`;
+  }
+);
 </script>
 
 <template>
   <div>
-    <Header />
+    <Header :questionStatus="questionStatus" />
     <div>
       <Question :question="quiz.questions[currentQuestionIndex]" />
     </div>
+    <button @click="currentQuestionIndex++">Next Question</button>
   </div>
 </template>
