37f486a84a8f06581bb16e536d5aa94f878cb82d
Pau Mesonero <paumesonero@Paus-MacBook-Air.local>
2023-04-27 18:06:06 +0200

    bar styles done

--- src/components/CardQuiz.vue
+++ src/components/CardQuiz.vue
@@ -2,7 +2,7 @@
 import Question from "./Question.vue";
 import Header from "./QuizHeader.vue";
 import { useRoute } from "vue-router";
-import { ref, watch } from "vue";
+import { ref, watch, computed } from "vue";
 import quizes from "../data/quizes.json";
 const route = useRoute();
 
@@ -10,20 +10,34 @@ const quizId = Number(route.params.id);
 const currentQuestionIndex = ref(0);
 const quiz = quizes.find((el) => el.id === quizId);
 
-const questionStatus = ref(
-  `${currentQuestionIndex.value}/${quiz.questions.length}`
-);
-watch(
-  () => currentQuestionIndex.value,
-  () => {
-    questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`;
+// const questionStatus = ref(
+//   `${currentQuestionIndex.value}/${quiz.questions.length}`
+// );
+// watch(
+//   () => currentQuestionIndex.value,
+//   () => {
+//     questionStatus.value = `${currentQuestionIndex.value}/${quiz.questions.length}`;
+//   }
+// );
+watch(currentQuestionIndex, () => {
+  if (currentQuestionIndex.value > quiz.questions.length) {
+    currentQuestionIndex.value = 0;
   }
-);
+});
+
+// this computed function can substitud the watch above
+const questionStatus = computed(() => {
+  return `${currentQuestionIndex.value}/${quiz.questions.length}`;
+});
+
+const barPercentage = computed(() => {
+  return `${(currentQuestionIndex.value / quiz.questions.length) * 100}% `;
+});
 </script>
 
 <template>
   <div>
-    <Header :questionStatus="questionStatus" />
+    <Header :questionStatus="questionStatus" :barPercentage="barPercentage" />
     <div>
       <Question :question="quiz.questions[currentQuestionIndex]" />
     </div>
